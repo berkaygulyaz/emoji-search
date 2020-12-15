7708b40e6a275c0aaf3ef87786eba614aa980350
Berkay Gülyaz <berkaygulyaz@gmail.com>
2020-12-15 21:12:00 +0300

    App design completed

---
+++ src/components/emojis/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Emojis = () => {
+    return (
+        <div className="emojis-wrapper">
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+            <div className="emoji">:)</div>
+        </div>
+    )
+}
+
+export default Emojis;
---
+++ src/components/search/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <input type="text" className="search-input" />
+    )
+}
+
+export default Search;
